e9d4df6ef6d0fc7eca3c1e4e547af224a4ed7b3c
badmanmusic007 <badmanmusic007@gmail.com>
2026-01-05 14:17:16 +0300

    fix: Restore missing DATA_URLS in config

--- src/config.py
+++ src/config.py
@@ -10,6 +10,41 @@ class Config:
     DATA_DIR.mkdir(exist_ok=True)
     MODELS_DIR.mkdir(exist_ok=True)
 
+    # Data URLs
+    DATA_URLS = [
+        # Upcoming Fixtures (All Leagues)
+        "https://www.football-data.co.uk/fixtures.csv",
+
+        # 2025/2026 Season (Current)
+        "https://www.football-data.co.uk/mmz4281/2526/E0.csv",  # Premier League
+        "https://www.football-data.co.uk/mmz4281/2526/E1.csv",  # Championship
+        "https://www.football-data.co.uk/mmz4281/2526/SP1.csv", # La Liga
+        "https://www.football-data.co.uk/mmz4281/2526/D1.csv",  # Bundesliga
+        "https://www.football-data.co.uk/mmz4281/2526/I1.csv",  # Serie A
+        "https://www.football-data.co.uk/mmz4281/2526/F1.csv",  # Ligue 1
+        "https://www.football-data.co.uk/mmz4281/2526/N1.csv",  # Eredivisie
+        "https://www.football-data.co.uk/mmz4281/2526/P1.csv",  # Liga Portugal
+        "https://www.football-data.co.uk/mmz4281/2526/SC0.csv", # Premiership
+
+        # 2024/2025 Season (History)
+        "https://www.football-data.co.uk/mmz4281/2425/E0.csv",
+        "https://www.football-data.co.uk/mmz4281/2425/E1.csv",
+        "https://www.football-data.co.uk/mmz4281/2425/SP1.csv",
+        "https://www.football-data.co.uk/mmz4281/2425/D1.csv",
+        "https://www.football-data.co.uk/mmz4281/2425/I1.csv",
+        "https://www.football-data.co.uk/mmz4281/2425/F1.csv",
+        "https://www.football-data.co.uk/mmz4281/2425/N1.csv",
+        "https://www.football-data.co.uk/mmz4281/2425/P1.csv",
+        "https://www.football-data.co.uk/mmz4281/2425/SC0.csv",
+
+        # 2023/2024 Season (History)
+        "https://www.football-data.co.uk/mmz4281/2324/E0.csv",
+        "https://www.football-data.co.uk/mmz4281/2324/E1.csv",
+        "https://www.football-data.co.uk/mmz4281/2324/SP1.csv",
+        "https://www.football-data.co.uk/mmz4281/2324/D1.csv",
+        "https://www.football-data.co.uk/mmz4281/2324/I1.csv",
+    ]
+
     # Model Settings
     FEATURES = [
         "home_form", "away_form", "home_home_form", "away_away_form", "h2h_home_wins", 
